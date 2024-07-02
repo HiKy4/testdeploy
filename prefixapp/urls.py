bda3e5988786edaadee142f6461e26215aa11259
MinhNhut <102950221+HiKy4@users.noreply.github.com>
2024-07-02 17:50:35 +0700

    Update urls.py

--- prefixapp/urls.py
+++ prefixapp/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('show/', views.show_patterns, name='show_patterns'),
+    path('patterns/', views.suggest_products, name='suggest_products'),
 ]
